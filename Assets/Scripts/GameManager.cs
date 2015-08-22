a236fa8cb20caf048efef3bc6b0389105216eaea
Даниил Пендиков <danilpendikov@gmail.com>
2015-08-22 21:12:33 +0600

    added game UI and start screen

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager {
+
+	public static void startGame () {
+
+		Application.LoadLevel ("Scene");
+
+		Player.charge = 1.0f;
+		Player.lives = Player.MAX_LIVES;
+		Player.score = 0;
+	}
+
+	public static void restart() {
+		Application.LoadLevel ("Start Scene");
+	}
+}
---
+++ Assets/Scripts/StartSceneUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartSceneUI : MonoBehaviour {
+
+	public Texture2D startButton;
+	public Texture2D circlesTexture;
+
+	void Start() {
+		GUI.skin.button.normal.background = startButton;
+		GUI.skin.button.hover.background = startButton;
+
+	}
+
+	void OnGUI () {
+		float cx = (Screen.width - circlesTexture.width) / 2.0f;
+		float cy = (Screen.height - circlesTexture.height) / 2.0f;
+		Rect circlesRect = new Rect(cx, cy, circlesTexture.width, circlesTexture.height);
+		GUI.DrawTexture (circlesRect, circlesTexture);
+
+		float sbw = 189;
+		float sbh = 135;
+
+		float sbx = (Screen.width - sbw) / 2.0f+6;
+		float sby = (Screen.height - sbh) / 2.0f;
+		Rect sbRect = new Rect (sbx, sby, sbw, sbh);
+
+		Event e = Event.current;
+
+		if (sbRect.Contains (e.mousePosition)) {
+			GUI.color = new Color(1f, 1f, 1f, 0.5f);
+			if(e.type == EventType.MouseUp ) {
+				print ("click");
+				startGameButtonPressed();
+			}
+		} 
+		GUI.DrawTexture (sbRect, startButton);
+	}
+
+	void startGameButtonPressed() {
+
+		GameManager.startGame ();
+
+	}
+}
